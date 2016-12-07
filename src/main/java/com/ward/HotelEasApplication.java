c6d9c1cb7f4aead8d5d70809de4ca8a1888f2810
Troy <tward4@tulane.edu>
2016-12-06 20:20:01 -0500

    Initial import

---
+++ src/main/java/com/ward/HotelEasApplication.java
@@ -0,0 +1,12 @@
+package com.ward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelEasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelEasApplication.class, args);
+	}
+}
