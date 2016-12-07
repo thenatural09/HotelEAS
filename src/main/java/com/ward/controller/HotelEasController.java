7fff2d49050bb75a98d9a2cb3a143a205b6df813
Troy <tward4@tulane.edu>
2016-12-06 20:27:41 -0500

    Creating classes and files

---
+++ src/main/java/com/ward/controller/HotelEasController.java
@@ -0,0 +1,7 @@
+package com.ward.controller;
+
+/**
+ * Created by Troy on 12/6/16.
+ */
+public class HotelEasController {
+}
---
+++ src/main/java/com/ward/services/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.ward.services;
+
+/**
+ * Created by Troy on 12/6/16.
+ */
+public interface RoomRepository {
+}
