9c53175d9679370508fa388154c0b0e4fb995848
Troy <tward4@tulane.edu>
2016-12-07 21:12:04 -0500

    Working on routes

--- src/main/java/com/ward/controller/HotelEasController.java
+++ src/main/java/com/ward/controller/HotelEasController.java
@@ -1,6 +1,8 @@
 package com.ward.controller;
 
+import com.ward.entities.User;
 import com.ward.services.*;
+import com.ward.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,5 +37,32 @@ public class HotelEasController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home(Model model, HttpSession session) {
         return "home";
+    }
+
+    @RequestMapping(path = "/signup", method = RequestMethod.POST)
+    public String signup(String username,String password,HttpSession session) throws Exception {
+        User user = new User(username, PasswordStorage.createHash(password));
+        if (user.getUsername() == null || user.getPassword() == null) {
+            throw new Exception("Invalid signup try");
+        }
+        users.save(user);
+        session.setAttribute("username",username);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(String username,String password,HttpSession session) throws Exception {
+        User user = users.findFirstByUsername(username);
+        if (user == null) {
+            throw new Exception("Invalid login try");
+        }
+        else if (user.getUsername() == null || user.getPassword() == null) {
+            throw new Exception("Invalid login try");
+        }
+        else if (!PasswordStorage.verifyPassword(password,user.getPassword())) {
+            throw new Exception("Wrong password");
+        }
+        session.setAttribute("username",username);
+        return "redirect:/";
     }
 }
--- src/main/java/com/ward/services/UserRepository.java
+++ src/main/java/com/ward/services/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by Troy on 12/6/16.
  */
 public interface UserRepository extends CrudRepository<User,Integer> {
+    User findFirstByUsername(String username);
 }
